e3b91941c9f67d6761edd23f3096eb8d8c51ef80
Aurous <ali.hasan@post.com>
2022-02-25 10:01:46 +0000

    Removed line in conn file that caused errors

--- php/src/includes/conn.inc.php
+++ php/src/includes/conn.inc.php
@@ -4,5 +4,4 @@ $user = "MYSQL_USER";
 $password = "MYSQL_PASSWORD";
 $database = 'MYSQL_DATABASE';
 $mysqli = new mysqli($hostname, $user, $password, $database);
-$mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT); // This gives more accurate error codes
-?>
+?>  
