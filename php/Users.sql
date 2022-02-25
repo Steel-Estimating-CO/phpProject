dab13b193c21dcf4f44776b2e8dfbd8587f7ff2a
Aurous <ali.hasan@post.com>
2022-02-25 09:54:42 +0000

    Added MarketPlace and inital Index file + Updated SQL File to reflect current database

---
+++ php/Users.sql
@@ -0,0 +1,73 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.3
+-- https://www.phpmyadmin.net/
+--
+-- Host: db
+-- Generation Time: Feb 25, 2022 at 09:50 AM
+-- Server version: 8.0.28
+-- PHP Version: 8.0.15
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `MYSQL_DATABASE`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Users`
+--
+
+CREATE TABLE `Users` (
+  `userID` int NOT NULL,
+  `Firstname` varchar(40) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `Lastname` varchar(40) NOT NULL,
+  `Email` varchar(40) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `Username` varchar(40) NOT NULL,
+  `Usertype` varchar(40) NOT NULL,
+  `Auth` tinyint DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `Users`
+--
+
+INSERT INTO `Users` (`userID`, `Firstname`, `Lastname`, `Email`, `Username`, `Usertype`, `Auth`) VALUES
+(23, 'Matt', 'dasas', 'lol@gmail.com', 'haha', 'Estimator', 0),
+(24, 'Steve', 'dasas', 'lol@gmail.com', 'haha', 'Estimator', 0),
+(25, 'Steve', 'dasas', 'lol@gmail.com', 'haha', 'Estimator', 0),
+(26, 'Steve', 'dasas', 'lol@gmail.com', 'haha', 'Estimator', 0);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `Users`
+--
+ALTER TABLE `Users`
+  ADD PRIMARY KEY (`userID`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `Users`
+--
+ALTER TABLE `Users`
+  MODIFY `userID` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
--- php/src/includes/conn.inc.php
+++ php/src/includes/conn.inc.php
@@ -4,6 +4,5 @@ $user = "MYSQL_USER";
 $password = "MYSQL_PASSWORD";
 $database = 'MYSQL_DATABASE';
 $mysqli = new mysqli($hostname, $user, $password, $database);
-
-
+$mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT); // This gives more accurate error codes
 ?>
