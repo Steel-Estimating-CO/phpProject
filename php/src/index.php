50d80d2ed0850aa10e84e8b50cdd402da226a2bc
Aurous <ali.hasan@post.com>
2022-04-01 11:25:13 +0100

    changed index style

--- php/src/index.php
+++ php/src/index.php
@@ -4,14 +4,16 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="indexLayout.css" type="text/css">
+    <link rel="stylesheet" href="EstimatorLayout.css" type="text/css">
     <title>Document</title>
 </head>
 <body>
-<ul class = "ul">
-  <li class ="li a"><a class="li" a href="LoginPage.php">Estimator Login</a></li>
-  <li class ="li a"><a href="AdminLogin.php">Admin Login</a></li>
-  <li class ="li a"><a href="CustomerLogin.php">Client Login</a></li>
-</ul>
+<div class="MenuBar">
+    <p class ="MenuHeader"> Homepage </p>
+    <div class="MenuOptions1" onclick="location.href='LoginPage.php'"><p class="MenuTxt">Estimator Login</p></div>
+    <div class="MenuOptions" onclick="location.href='AdminLogin.php'"><p class="MenuTxt">Admin Login</p></div>
+    <div class="MenuOptions" onclick="location.href='CustomerLogin.php'"><p class="MenuTxt">Client Login</p></div>
+    <div class="MenuOptions" onclick="location.href='register.php'"><p class="MenuTxt">Register</p></div>
+    </div>
 </body>
 </html>
